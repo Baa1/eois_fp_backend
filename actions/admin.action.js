257e1066346db841f7f409fc63c5c4519048b5e8
Sergeu <kvezee01@gmail.com>
2021-11-16 01:52:05 +0700

    пост запрос

--- actions/admin.action.js
+++ actions/admin.action.js
@@ -1,4 +1,6 @@
 const db = require('../models')
+const jwt = require('jsonwebtoken')
+const { ROLES } = require('../utils/enums')
 
 exports.addSession = () => {
 
@@ -13,5 +15,18 @@ exports.getSession = () => {
 }
 
 exports.addProject = (res, projectData) => {
-
+await db.transaction(async transaction => {
+		const { name, description } = projectData
+    console.log(userData)
+    
+	project = await db.Project.create({
+		name,
+		description
+	}, { transaction })
+		
+	res.result = {
+		nmae: project.name,
+		description: project.description
+    }
+	})
 }
