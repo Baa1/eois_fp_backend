e6a42fb755bb89e1e6701c2401e9478cbc6a7d99
Sergeu <kvezee01@gmail.com>
2021-11-16 20:29:59 +0700

    миграция

--- migrations/20211116113617-project-table.js
+++ migrations/20211116113617-project-table.js
@@ -2,20 +2,45 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.createTable('Project', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      creatorId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'User',
+            key: 'id'
+        }
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      likes: {
+          allowNull: true,
+          type: Sequelize.INTEGER
+      },
+      dislikes: {
+          allowNull: true,
+          type: Sequelize.INTEGER
+      }
+    })
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('Project')
   }
 };
